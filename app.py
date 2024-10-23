e9cb438c28c94740e36b1299a22eea65765d1f9c
SreeHarshan <sreeerode12@gmail.com>
2024-10-23 21:41:07 +0530

    working flask server with basic get requests

---
+++ app.py
@@ -0,0 +1,41 @@
+from flask import Flask
+import subprocess
+import os
+from pathlib import Path
+
+app = Flask("ServerMon")
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+@app.route("/heartbeat")
+def heartbeat():
+    return {"HeartBeat":True}
+
+@app.route("/ssdtemp")
+def ssd_temp():
+
+    cwd = os.getcwd()
+    file = "/tmp/ssd_temp"
+    relpath = os.path.relpath(file,cwd)
+
+    if(not os.path.exists(relpath)):
+        print(relpath)
+        return {"ERROR":"Path does not exist"}
+    with open(relpath) as f: 
+        val = f.readline()[:-1]
+        f.close()
+
+    return {"Temp":val}
+
+@app.route("/storage")
+def storage():
+    out = subprocess.run(['df','/','--output=size,pcent'],stdout=subprocess.PIPE).stdout.decode('utf-8')
+
+    tot,percent = out.split("\n")[1].split()
+
+    return {"Total":tot,"Percent":percent}
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
